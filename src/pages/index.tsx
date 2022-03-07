d8d9ffeb588884b41c41e54cde3a9923d83f4787
Tiago Rosa <96127800+TiagoR99@users.noreply.github.com>
2022-03-07 17:44:33 +0000

    Formik

---
+++ src/pages/index.tsx
@@ -0,0 +1,45 @@
+import {
+  Routes,
+  Route,
+  Navigate,
+  useLocation
+} from "react-router-dom";
+import React from "react";
+import { useAppContext } from "../context/AppContext";
+import Home from "./home";
+import Login from "./login";
+
+const ProtectRoute = ({ children }: { children: JSX.Element }) => {
+  const { state } = useAppContext();
+  const location = useLocation();
+  if (!state.auth.isAuthenticated) {
+    console.log("Redirecting to login...");
+    return <Navigate to="/login" state={{ from: location }} replace />
+  }
+  else {
+    return children
+  }
+}
+
+const Pages = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/" element={
+        <ProtectRoute>
+          <Home />
+        </ProtectRoute>
+      } />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
+  )
+};
+
+export default Pages
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,18 @@
-import { useMutation } from "@apollo/react-hooks";
+import { fromPromise, useMutation } from "@apollo/react-hooks";
 import { Navigate, useLocation, useNavigate } from "react-router-dom";
-import { login, loginVariables } from "../api/graphQLModel";
+import { login, LoginUserInput, loginVariables } from "../api/graphQLModel";
 import { LOGIN } from "../api/graphQLQueries";
 import { useAppContext } from "../context/AppContext";
+import './forms.css'
+import { useFormik } from "formik";
+import * as Yup from 'yup'
+import { Button, TextField } from "@mui/material";
+import { fork } from "child_process";
+
+  const initialValues: LoginUserInput = {
+      username:"",
+      password:"",
+    }
 
 const Login = (props: any) => {
   const [loginMutation] = useMutation<login, loginVariables>(LOGIN)
@@ -10,17 +20,12 @@ const Login = (props: any) => {
   const location = useLocation()
   const navigate = useNavigate()
 
-  // if user tries to login while being authenticated
-  if (state.auth.isAuthenticated) {
-    return <Navigate to="/" replace />
-  }
-
-  const onSubmit = async () => {
+  const onSubmit = async (values: LoginUserInput) => {
     await loginMutation({
       variables: {
         loginInput: {
-          password: "12345",
-          username: "administrador@sevways.cloud"
+          password: values.password,
+          username: values.username
         }
       }
     }).then((res) => {
@@ -37,16 +42,64 @@ const Login = (props: any) => {
     })
   }
 
+  const formik = useFormik({
+    initialValues: initialValues,
+    validationSchema: Yup.object({
+      username: Yup.string()
+          .email("Email inválido")
+          .max(50,"Só pode conter no máximo 50 caracteres.")
+          .required("É necessário inserir um email."),
+      password: Yup.string()
+          .min(4, "Password muito pequena")
+          .required("É necessário introduzir uma password.")
+          .matches(/[0-9]/, 'Password só pode conter letras.'),  
+    }),
+    onSubmit: onSubmit
+  })
+  console.log(formik.errors)
+
+  // if user tries to login while being authenticated
+  if (state.auth.isAuthenticated) {
+    return <Navigate to="/" replace />
+  }
+
   return (
     <>
-      <h1>Login</h1>
-      <button
-        onClick={() => {
-          onSubmit()
-        }}
-      >
-        Fake login
-      </button>
+      <h1 className="title">Login</h1>
+      <div className="main">
+        <form onSubmit={formik.handleSubmit}>
+          <div className="main">
+            <TextField 
+                fullWidth
+                id="username"
+                name="username"
+                type="text"
+                placeholder="Email"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value = {formik.values.username}
+                error={!!formik.touched.username && !!formik.errors.username}
+
+                      helperText={formik.touched.username && formik.errors.username}
+            /><br/>
+            <label className="text">Password: </label>
+            <TextField 
+                fullWidth
+                id="password"
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value = {formik.values.password}
+                error={!!formik.touched.password && !!formik.errors.password}
+
+                      helperText={formik.touched.password && formik.errors.password}
+            />
+          </div>
+          <Button className="submit" type="submit">Submit</Button>
+        </form>
+      </div>
     </>
   );
 }
